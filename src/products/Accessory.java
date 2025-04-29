54910261d43d6144a55f55ee220e5457398977bb
Fatemeh Rajabzade <ra.fatemeh851@gmail.com>
2025-04-29 12:09:02 -0700

    Add Accessory class

--- src/products/Accessory.java
+++ src/products/Accessory.java
@@ -1,4 +1,31 @@
 package products;
 
-public class Accessory {
+public class Accessory extends Product{
+    private String color;
+    private static int idCounterAccessory = 3001;
+
+
+    public Accessory(String title, Double price, String color){
+        super(title, price);
+        this.color = color;
+    }
+
+    //getter & setter
+    public String getColor(){
+        return color;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + " Color: " + color ;
+    }
+
+    @Override
+    protected String generateId(){
+        return String.format("%04d", idCounterAccessory++);
+    }
 }
